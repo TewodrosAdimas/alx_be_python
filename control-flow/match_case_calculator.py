aafc6f44e177f0e1bc1d441669944f4265490f38
TewodrosAdimas <yosefenadagim@gmail.com>
2024-06-26 17:21:26 +0300

    zero division error handled

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,19 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input ("Choose the operation (+, -, *, /): ")
+
+try:
+
+    match operation:
+        case "+":
+            result = num1 + num2
+        case "-":
+            result = num1 - num2
+        case "*":
+            result = num1 * num2
+        case "/":
+            result = num1 / num2
+    print(f"The result is {result}.")
+
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
