5defe8ea87b0ee1a81bd032ec5f3e8818366edb5
TewodrosAdimas <yosefenadagim@gmail.com>
2024-07-06 20:11:05 +0300

    shop

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,7 +12,6 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input ('\"Enter the item to add:\" ')
             shopping_list.append(input ("\"Enter the item to add:\"" ))
             
         elif choice == '2':
