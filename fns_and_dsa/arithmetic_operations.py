023251d04e338d26db8f94a00b0d184dbde0666b
TewodrosAdimas <yosefenadagim@gmail.com>
2024-07-06 19:32:45 +0300

    function with match

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,11 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            return (num1 + num2)
-        case "subtract":
-            return(num1 - num2)
-        case "multiply":
-            return(num1 * num2)
-        case "divide":
+    if operation == "add":
+        return (num1 + num2)
+    elif operation == "subtract":
+        return (num1 - num2)
+    elif operation == "multiply":
+        return (num1 * num2)
+    else :
             try :
                 return (num1 / num2)
 
