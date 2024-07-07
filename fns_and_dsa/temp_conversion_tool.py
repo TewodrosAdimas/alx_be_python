25fe2ae3c8cb57d7965c99a755995f0f94220cc7
TewodrosAdimas <yosefenadagim@gmail.com>
2024-07-07 14:18:37 +0300

    temp

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,7 +2,7 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    celsius = FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32)
+    celsius = (FAHRENHEIT_TO_CELSIUS_FACTOR - 32) * fahrenheit
     print(f"{fahrenheit}°F is {celsius:.2f}°C")
 
 def convert_to_fahrenheit(celsius):
