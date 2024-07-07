011825324611216e77cb8e528e990e1ef570613f
TewodrosAdimas <yosefenadagim@gmail.com>
2024-07-07 10:33:53 +0300

    date cal

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,13 +1,16 @@
-from datetime import *
+import datetime
+def display_current_datetime():
+    current_dat = datetime.datetime.now()
+    current_date = current_dat.strftime("%Y-%m-%d %H:%M:%S")
+    print ("Current date and time: ", current_date) #"YYYY-MM-DD HH:MM:SS."
 
-# def display_current_datetime():
-#     current_date = 9
-#     print ("Current date and time: ", current_date) #"YYYY-MM-DD HH:MM:SS."
+def calculate_future_date():
+    add = int(input("Enter the number of days to add to the current date: "))
+    delta = datetime.timedelta(days=add)
+    print(delta)
+    current_date = datetime.date.today()
+    future_date = current_date + delta
+    print("Future date:", future_date)  #YYYY-MM-DD
 
-# def calculate_future_date():
-#     add = print("Enter the number of days to add to the current date: ")
-#     future_date = add 
-#     print("Future date:", future_date)  #YYYY-MM-DD
-
-x = date(2024, 5, 21)
-print(x)
+display_current_datetime()
+calculate_future_date()
