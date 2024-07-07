c11132df8c7f4a24421c31d10cab1268d58b7154
TewodrosAdimas <yosefenadagim@gmail.com>
2024-07-07 09:44:12 +0300

    shop

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,13 @@
+from datetime import *
+
+# def display_current_datetime():
+#     current_date = 9
+#     print ("Current date and time: ", current_date) #"YYYY-MM-DD HH:MM:SS."
+
+# def calculate_future_date():
+#     add = print("Enter the number of days to add to the current date: ")
+#     future_date = add 
+#     print("Future date:", future_date)  #YYYY-MM-DD
+
+x = time()
+print(x)
