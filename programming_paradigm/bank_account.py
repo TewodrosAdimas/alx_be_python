23d58a946250d34541d4956d15cf9116e009f2b5
TewodrosAdimas <yosefenadagim@gmail.com>
2024-07-17 07:31:29 +0300

    oop

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,5 +1,5 @@
 class BankAccount():
-    def __init__(self, account_balance):
+    def __init__(self,account_balance = 0):
         self.account_balance = account_balance
 
     def deposit(self,amount):
@@ -9,4 +9,4 @@ class BankAccount():
         return self.account_balance >= amount
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print (f"Current Balance: ${self.account_balance}")
